66d9495aa8a931c9b614287d2aeb1465973656c4
Your Name <you@example.com>
2025-05-17 22:34:02 +0900

    p005

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 use polars::prelude::*;
+use std::fs::File;
+
 
 pub fn p_001() -> PolarsResult<()> {
     let file = "./data/receipt.csv";
@@ -12,7 +14,6 @@ pub fn p_001() -> PolarsResult<()> {
     Ok(())
 }
 
-
 pub fn p_002() -> PolarsResult<()> {
     let file = "./data/receipt.csv";
     let df = LazyCsvReader::new(file)
@@ -24,7 +25,6 @@ pub fn p_002() -> PolarsResult<()> {
     println!("{:?}", df);
     Ok(())
 }
-
 
 pub fn p_003() -> PolarsResult<()> {
     let file = "./data/receipt.csv";
@@ -41,7 +41,6 @@ pub fn p_003() -> PolarsResult<()> {
     Ok(())
 }
 
-
 pub fn p_004() -> PolarsResult<()> {
     let file = "./data/receipt.csv";
     
@@ -53,5 +52,26 @@ pub fn p_004() -> PolarsResult<()> {
         .collect()?;
 
     println!("{:?}", df);
+    Ok(())
+}
+
+pub fn p_005() -> PolarsResult<()> {
+    let file = "./data/receipt.csv";
+
+    let mut df = LazyCsvReader::new(file)
+        .finish()?
+        .select([col("sales_ymd"), col("customer_id"), col("product_cd"), col("amount")])
+        .filter(col("customer_id").eq(lit("CS018205000001")))  // INFO: 250517 lit がないと落ちる。Lazy 特有？lit や col は polars では付けるとしておけばいいかな？
+        .filter(col("amount").gt_eq(lit(1000)))
+        .collect()?;
+    println!("{:?}", df);
+
+    // INFO: 250517 csv でデータを吐いて、中身を確認する。
+    // ref: https://polars-ja.github.io/docs-ja/user-guide/getting-started/#_2
+    CsvWriter::new(File::create("./misc/p_005.csv")?)
+        .include_header(true)
+        .include_bom(true)  // INFO: 250517 utf8 で吐き出すと思うので、bom 付けないとエクセルで開けないハズ。
+        .finish(&mut df)?;
+
     Ok(())
 }

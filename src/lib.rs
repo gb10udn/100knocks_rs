330f87bf0802fa84fedb7b5ff0439c7a91e2c664
Your Name <you@example.com>
2025-05-17 21:21:24 +0900

    p-004

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,8 @@ pub fn p_002() -> PolarsResult<()> {
     let df = LazyCsvReader::new(file)
         .with_n_rows(Some(10))
         .finish()?
-        .collect()?
-        .select(["sales_ymd", "customer_id", "product_cd", "amount"])?;
+        .select([col("sales_ymd"), col("customer_id"), col("product_cd"), col("amount")])  //INFO: 250517 .collect() 前で呼び出すと最適化がかかるみたい。
+        .collect()?;
 
     println!("{:?}", df);
     Ok(())
@@ -31,12 +31,27 @@ pub fn p_003() -> PolarsResult<()> {
     let mut df = LazyCsvReader::new(file)
         .with_n_rows(Some(10))
         .finish()?
-        .collect()?
-        .select(["sales_ymd", "customer_id", "product_cd", "amount"])?;
+        .select([col("sales_ymd"), col("customer_id"), col("product_cd"), col("amount")])
+        .collect()?;
          
     let df = df.rename("sales_ymd", "sales_ymdsales_ymd".into())?;  // INFO: 250517 .rename() は、メソッドチェーンの中で使用不可。
 
     println!("{:?}", df);
 
+    Ok(())
+}
+
+
+pub fn p_004() -> PolarsResult<()> {
+    let file = "./data/receipt.csv";
+    
+    let df = LazyCsvReader::new(file)
+        .with_has_header(true)
+        .finish()?
+        .select([col("sales_ymd"), col("customer_id"), col("product_cd"), col("amount")])
+        .filter(col("customer_id").eq(lit("CS018205000001")))
+        .collect()?;
+
+    println!("{:?}", df);
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main()  {
     // polars_test::p_001().unwrap();
     // polars_test::p_002().unwrap();
-    polars_test::p_003().unwrap();
+    // polars_test::p_003().unwrap();
+    polars_test::p_004().unwrap();
 }
